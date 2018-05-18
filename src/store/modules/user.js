34458012928caa56db6dd7df68fe07bf7a044abe
jaak <zhangykvip@126.com>
2018-05-18 11:09:38 +0800

    change avatar

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,7 +4,7 @@ const user = {
   state: {
     token: getToken(),
     name: '',
-    avatar: '',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
     roles: []
   },
 
